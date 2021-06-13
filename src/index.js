36e4f9caf67b2e9d0ddb725326ade681d598188a
Pete Low <pedritolow@gmail.com>
2021-06-13 15:00:41 -0700

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,21 +6,31 @@ class App extends React.Component {
         super(props);
         //this is the only time we do direct assignment
         //to this.state
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 //We called setState!!!
                 this.setState({ lat: position.coords.latitude })
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
         );
     };
 
     //React says we have to define render!!
     render() {
-        return <div>Latitude: {this.state.lat}</div>;
-    };
+       if (this.state.errorMessage && !this.state.lat){
+           return <div>Error: {this.state.errorMessage}</div>
+       }
+
+       if(!this.state.errorMessage && this.state.lat){
+           return <div>Lattitude: {this.state.lat}</div>
+       }
+
+       return <div>Loading</div>
+    }
 };
 
 ReactDOM.render(
