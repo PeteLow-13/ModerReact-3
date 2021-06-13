c7746c48e7763e39c427a3df8cf7adb8eacd730a
Pete Low <pedritolow@gmail.com>
2021-06-13 16:30:38 -0700

    added renderContent()

--- src/index.js
+++ src/index.js
@@ -13,19 +13,25 @@ class App extends React.Component {
             err => this.setState({ errorMessage: err.message })
         );
     }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+ 
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}  />
+        }
+ 
+        return <Loader message='Please accept location request'/>
+    }
     
 
     //React says we have to define render!!
     render() {
-       if (this.state.errorMessage && !this.state.lat){
-           return <div>Error: {this.state.errorMessage}</div>
-       }
-
-       if(!this.state.errorMessage && this.state.lat){
-           return <SeasonDisplay lat={this.state.lat}  />
-       }
-
-       return <Loader />
+       return (
+           <div className='border red'>{this.renderContent()}</div>
+       )
     }
 };
 
