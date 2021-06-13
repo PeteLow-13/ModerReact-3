3a116f810a238ac11a2d4df27061a6203c073a65
Pete Low <pedritolow@gmail.com>
2021-06-13 14:12:27 -0700

    added constructor and state

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        )
+    constructor(props) {
+        super(props);
+        //this is the only time we do direct assignment
+        //to this.state
+        this.state = { lat: null };
 
-        return <div>Latitude: </div>
-    }
-}
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //We called setState!!!
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => console.log(err)
+        );
+    };
+
+    //React says we have to define render!!
+    render() {
+        return <div>Latitude: {this.state.lat}</div>;
+    };
+};
 
 ReactDOM.render(
     <App /> , document.querySelector('#root')
